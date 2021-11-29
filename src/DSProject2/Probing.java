5c540fa42aa93f9f04436c0b214872d2e3ad893d
Bahaa <219110085@psu.edu.sa>
2021-11-29 14:13:46 +0300

    probing class search method - NOTE: my observation that it will not work as the key used is the same in multiple node, so we must find a unique key

--- src/DSProject2/Probing.java
+++ src/DSProject2/Probing.java
@@ -39,6 +39,59 @@ public class Probing {
     }
 
 
+    public Node searchConName(String key){
+        // Get the hashedKey:
+        int hashedIndex = Hashing.stringHash(key);
+
+        // Check Assuming No collisions:
+        if (probedTable[hashedIndex] != null && probedTable[hashedIndex].conName.equals(key)){ // index not empty and node matches the given key
+            return probedTable[hashedIndex]; // Found
+        }
+
+            /*
+                Assuming Collision Exists:
+                Linear probing: handles collisions by placing colliding item in next
+                (circularly) available table cell
+                if A[j] is already occupied; try A[j+1 mod N]; then A[j+2 mod N] … until free space
+            */
+
+        int originalHashedIndex = hashedIndex; // To use it in circling around
+
+        if (hashedIndex == probedTable.length-1){ // last index? circle around the array
+            hashedIndex = 0;
+        }
+        else{
+            hashedIndex++;
+        }
+
+        /*
+            search for the node in the next available indices given that it was inserted using probing:
+            To find the hashedIndex: 1) index must be not null
+                                     2) The key in Node must Match the given key
+                                     3) We must not circle back to the original index, if we did, then it does not exist
+         */
+
+        while (probedTable[hashedIndex] != null && (probedTable[hashedIndex].equals(key) == false)
+                && hashedIndex != originalHashedIndex){
+
+            hashedIndex = (hashedIndex+1) % probedTable.length;
+
+        }
+
+        // Check the found hashedIndex:
+
+        if (probedTable[hashedIndex] != null && probedTable[hashedIndex].equals(key)){
+            return probedTable[hashedIndex];
+        }
+        else {
+            return null; // Element not found
+        }
+    }
+
+
+
+
+
     // AUX:
 
     public void print() {
